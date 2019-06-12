f967d70b39903cd3aa62f27c88eab75bd168587d
Rob Conway <robconway12@gmail.com>
2019-06-11 21:30:38 -0700

    added jQuery and variable to API

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -1,22 +1,29 @@
-var https =require('https');
-var path = "/books/v1/volumes?q=isbn:1119056071&key=AIzaSyDB0TwWP5ouoFr37dmNo1r9HCRWzg3czsk";
+$("#add-btn").on('click', function() {
+	
+	var isbn = document.getElementById('isbn');
+	
+	var https = require('https');
+	var path = '/books/v1/volumes?q=isbn:' + isbn + '&key=AIzaSyDB0TwWP5ouoFr37dmNo1r9HCRWzg3czsk';
 
-var options = {
-  	host: 'www.googleapis.com',
-  	path: path,
+	var options = {
+		host: 'www.googleapis.com',
+		path: path,
 	};
 
-var str ='';
-var objectJSON;
+	var str = '';
+	var objectJSON;
 
-//Initiating the request that gets ISBN data 
-https.request(options, function(res){
-	res.on('data', function (chunk) {
-    	str += chunk;
-  		});
-  	res.on('end', function () {
-    	objectJSON=JSON.parse(str);
-        //prints the book object
-    	console.log(objectJSON.items[0]);
-  		});
-	}).end();
+	//Initiating the request that gets ISBN data
+	https
+		.request(options, function(res) {
+			res.on('data', function(chunk) {
+				str += chunk;
+			});
+			res.on('end', function() {
+				objectJSON = JSON.parse(str);
+				//prints the book object
+				console.log(objectJSON.items[0]);
+			});
+		})
+		.end();
+});
