db32d0dc2be5d768da5662634fdc3dda87db9c9c
Rob Conway <robconway12@gmail.com>
2019-06-15 22:11:57 -0700

    replaced node with jQuery, added results to HTML

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -11,75 +11,93 @@ $(document).ready(function() {
 			.trim();
 		var isbn = $('#isbn')
 			.val()
-			.trim();
+			.trim()
+			.replace(/\D/g, '');
 		console.log('title: ' + title + '\nauthor: ' + author + '\nISBN: ' + isbn);
 
-		if (isbn) {
-			var path = '/books/v1/volumes?q=isbn:' + isbn + '&key=' + key;
-		} else {
-			if (!title) {
-				var path = '/books/v1/volumes?q=inauthor:' + author + '&key=' + key;
-			} else if (!author) {
-				var path = '/books/v1/volumes?q=intitle:' + title + '&key=' + key;
+		function searchBook() {
+			var queryURL;
+
+			if (isbn) {
+				var queryURL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn + '&key=' + key;
 			} else {
-				var path = '/books/v1/volumes?q=intitle:' + title + '+inauthor:' + author + '&key=' + key;
+				if (!title) {
+					var queryURL = 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + author + '&key=' + key;
+				} else if (!author) {
+					var queryURL = 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title + '&key=' + key;
+				} else {
+					var queryURL =
+						'https://www.googleapis.com/books/v1/volumes?q=intitle:' +
+						title +
+						'+inauthor:' +
+						author +
+						'&key=' +
+						key;
+				}
 			}
+			queryURL = queryURL.replace(/ /g, '%20');
+			console.log(queryURL);
+
+			$.ajax({
+				url: queryURL,
+				method: 'GET',
+			}).then(function(response) {
+				// Printing the entire object to console
+				console.log(response);
+
+				// Empty the contents of the book-results div
+				$('#book-results').empty();
+
+				// Constructing HTML containing the book information
+				for (let i = 0; i < response.items.length; i++) {
+					var bookImage = $('<img>').attr(
+						'src',
+						'https://books.google.com/books?id=' +
+							response.items[i].id +
+							'&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+					);
+					var bookTitle = $('<h4>').text(response.items[i].volumeInfo.title);
+					var bookAuthor = $('<h5>').text(response.items[i].volumeInfo.authors[0]);
+					var bookGenre = $('<h5>').text(response.items[i].volumeInfo.categories[0]);
+					var bookDescription = $('<p>').text(response.items[i].volumeInfo.description);
+					var date = new Date(response.items[i].volumeInfo.publishedDate);
+					var bookYear = $('<p>').text('Published: ' + date.getFullYear());
+					var bookPages = $('<p>').text(response.items[i].volumeInfo.pageCount + 'pages');
+					var bookSelect = $("<button class='select-book'>")
+						.attr('href', '')
+						.text('Add this book');
+					var lineBreak = $('</br>');
+
+					// Append the new book content
+					$('#book-results').append(
+						lineBreak,
+						bookImage,
+						bookTitle,
+						bookAuthor,
+						bookGenre,
+						bookDescription,
+						bookYear,
+						bookPages,
+						bookSelect,
+					);
+				}
+			});
 		}
 
-		console.log(path);
-
-		var https = require('https');
-		var options = {
-			host: 'www.googleapis.com',
-			path: path,
-		};
-
-		var str = '';
-		var objectJSON;
-
-		//Initiating the request that gets book data
-		https
-			.request(options, function(res) {
-				res.on('data', function(chunk) {
-					str += chunk;
-				});
-				res.on('end', function() {
-					objectJSON = JSON.parse(str);
-					//prints the title of the book
-					console.log(objectJSON.items[0]);
-				});
-			})
-			.end();
-
-		//prints the book object
+		searchBook();
 		/*
-		var resISBN = isbn;
-		console.log(resISBN + '\n');
+		// Event handler for user clicking the select-book button
+		$('.select-book').on('click', function(event) {
+			// Preventing the button from trying to submit the form
+			event.preventDefault();
+			// Storing the book name
+			var inputbook = $('#book-input')
+				.val()
+				.trim();
 
-		var resTitle = objectJSON.items[0].volumeInfo.title;
-		console.log(resTitle + '\n');
-
-		var resAuthor = objectJSON.items[0].volumeInfo.authors[0];
-		console.log(resAuthor + '\n');
-
-		var resGenre = objectJSON.items[0].volumeInfo.categories[0];
-		console.log(resGenre + '\n');
-
-		var resDescription = objectJSON.items[0].volumeInfo.description;
-		console.log(resDescription + '\n');
-
-		var date = new Date(objectJSON.items[0].volumeInfo.publishedDate);
-		var resYear = date.getFullYear();
-		console.log(resYear + '\n');
-
-		var resPages = objectJSON.items[0].volumeInfo.pageCount;
-		console.log(resPages + '\n');
-
-		var resImage =
-			'https://books.google.com/books/content?id=' +
-			objectJSON[0].items[0].id +
-			'&printsec=frontcover&img=1&zoom=2&edge=nocurl&source=gbs_api';
-		console.log(resImage + '\n');
-		*/
+			// Running the searchBook function(passing in the book as an argument)
+			searchBook(inputbook);
+		});
+*/
 	});
 });
